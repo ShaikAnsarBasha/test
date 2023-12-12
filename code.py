a6b8591c5018613c688b463ccacbcc55901486cc
Shaik Ansar Basha <ansarbashaab7@gmail.com>
2023-12-12 23:03:36 +0530

    fresh

--- code.py
+++ code.py
@@ -1,12 +1,3 @@
-print("Hello World")
-print("Hi")
-a = True
-if a and False:
-    print("Irfan")
-    print("Irfan3")
-    print("Irfan4")
-    print("Irfan5")
-else:
-    print("Ansar")
-
-print("This is custom message")
+inp = True
+if inp:
+    print("a0")
